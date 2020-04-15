58911c31f24629f52e829ee2b7f6bf011ac5b492
Jérémie Roulin <jeremie.roulin@ideative.ch>
2020-04-15 21:13:08 +0200

    Add backend test

---
+++ backend/database/migrations/2020_04_15_190440_create_team_members_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeamMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('team_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('profession');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('team_members');
+    }
+}
