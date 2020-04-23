c53828606fc9fd195c1e9b309044d65780aa6274
Jérémie Roulin <jeremie.roulin@ideative.ch>
2020-04-23 09:58:51 +0200

    Add default records in database

--- pocovidscreen/database/seeds/DatabaseSeeder.php
+++ pocovidscreen/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('image_types')->insert(['name' => 'X-Ray']);
+        DB::table('image_types')->insert(['name' => 'CT']);
+        DB::table('image_types')->insert(['name' => 'POCUS']);
     }
 }
