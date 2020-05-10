463df8129718809ca68961c298c5dc54db2a083a
hebaishi <hebaishi.husam@gmail.com>
2020-05-10 22:38:43 +0100

    Change web root path to 'web_root'

--- pocovidscreen/config/filesystems.php
+++ pocovidscreen/config/filesystems.php
@@ -55,7 +55,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/web_root'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
