2c732ee9ae2e106ae1fb93b0a42d5f046ec935fc
Jérémie Roulin <roulin.jeremie@gmail.com>
2020-05-12 12:46:46 +0200

    Add lumiata logo

--- pocovidscreen/resources/ui/src/components/Partner/ParternList.js
+++ pocovidscreen/resources/ui/src/components/Partner/ParternList.js
@@ -5,6 +5,7 @@ import euvsvirus from './images/euvsvirus.svg'
 import hackzurich from './images/hackzurich.jpg'
 import backpack from './images/backpack.png'
 import checkdomain from './images/checkdomain.svg'
+import lumiata from './images/lumiata.svg'
 import {useFadeInOnScroll} from '../../utils/animation';
 
 const PartnerList = () => {
@@ -18,10 +19,13 @@ const PartnerList = () => {
                 <img src={hidora} alt="Hidora logo"/>
             </a>
             <a href="https://digitalfestival.ch/en/HACK/" target="_blank" className="partner col fadeIn my-4" title="Event website">
-                <img src={hackzurich} alt="The Hack Zurich logo" width="110"/>
+                <img src={hackzurich} alt="The Hack Zurich logo" width="90"/>
             </a>
             <a href="https://euvsvirus.org/" target="_blank" className="partner col fadeIn my-4" title="Event website">
-                <img src={euvsvirus} alt="The EUvsVirus logo" width="180"/>
+                <img src={euvsvirus} alt="The EUvsVirus logo" width="160"/>
+            </a>
+            <a href="https://www.lumiata.com/" target="_blank" className="partner col fadeIn my-4" title="Event organizer website">
+                <img src={lumiata} alt="The Lumiata logo" width="110"/>
             </a>
             <a href="https://www.google.com/" target="_blank" className="partner col fadeIn my-4" title="Our cloud hosting provider.">
                 <img src={google} alt="Google logo"/>
