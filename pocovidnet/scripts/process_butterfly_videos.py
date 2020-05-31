4a31c892f89761cd6d76f1eff9559d0644e6b940
Jannis Born <jab@zurich.ibm.com>
2020-05-31 02:56:36 +0200

    model genesis added

--- pocovidnet/scripts/process_butterfly_videos.py
+++ pocovidnet/scripts/process_butterfly_videos.py
@@ -1,13 +1,11 @@
 from pocovidnet.utils_butterfly_data import (
     get_processing_info, get_paths, label_to_dir
 )
-from skvideo import io
 import os
 import cv2
 import numpy as np
 
 import argparse
-import os
 
 if __name__ == "__main__":
 
