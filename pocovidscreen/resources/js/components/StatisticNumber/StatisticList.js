fe680219b350f2b75a1e826b011094f5012a5191
Jannis Born <jab@zurich.ibm.com>
2020-08-08 12:03:07 +0200

    upd: Statistics updated

--- pocovidscreen/resources/js/components/StatisticNumber/StatisticList.js
+++ pocovidscreen/resources/js/components/StatisticNumber/StatisticList.js
@@ -1,5 +1,5 @@
-import React, {useRef} from 'react';
-import {useFadeInOnScroll} from '../../utils/animation';
+import React, { useRef } from 'react';
+import { useFadeInOnScroll } from '../../utils/animation';
 import StatisticNumber from './StatisticNumber';
 
 const StatisticNumberList = (props) => {
@@ -10,13 +10,13 @@ const StatisticNumberList = (props) => {
     return (
         <div className="row pt-5" ref={statsRef}>
             <div className="col-8 col-md offset-lg-1 fadeIn">
-                <StatisticNumber className="primary" number="64" text="videos edited by our data scientists."/>
+                <StatisticNumber className="primary" number=">160" text="videos edited by our data scientists." />
             </div>
             <div className="col-8 col-md fadeIn">
-                <StatisticNumber className="secondary" number="1103" text="images that trained the AI."/>
+                <StatisticNumber className="secondary" number=">2500" text="images that trained the AI." />
             </div>
             <div className="col-8 col-md fadeIn">
-                <StatisticNumber className="primary" number="96%" text="sensititvity of our AI on COVID-19 cases."/>
+                <StatisticNumber className="primary" number="96%" text="sensititvity of our AI on COVID-19 cases." />
             </div>
         </div>
     );
--- pocovidscreen/resources/js/pages/Home/Home.js
+++ pocovidscreen/resources/js/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Text from '../../components/Content/Text';
 import TextImage from '../../components/Content/TextImage';
 import CallToAction from '../../components/CallToAction/CallToAction';
@@ -6,7 +6,7 @@ import gsap from 'gsap';
 import covid from '../../assets/images/covid19.png'
 import pocus from '../../assets/images/pocus-covid.gif'
 import VideoPresentation from '../../components/VideoPresentation/VideoPresentation';
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import Slider from '../../components/Slider/Slider';
 import StatisticList from '../../components/StatisticNumber/StatisticList';
 import PartnerList from '../../components/Partner/ParternList';
@@ -14,8 +14,8 @@ import Layout from '../Layout';
 import Teaser from '../../components/Teaser/Teaser';
 import configuration from '../../utils/constants';
 import Modal from '../../components/Modal/Modal';
-import {acceptDisclaimer, AppContext} from '../../context/AppContext';
-import {useHistory} from 'react-router-dom';
+import { acceptDisclaimer, AppContext } from '../../context/AppContext';
+import { useHistory } from 'react-router-dom';
 
 const Home = () => {
 
@@ -50,13 +50,13 @@ const Home = () => {
 
     let tl = gsap.timeline();
     useEffect(() => {
-        gsap.to('.page', 0, {visibility: 'visible'});
-        tl.from('.teaser-wrapper', 1.5, {y: 500, opacity: 0, ease: 'power4.out'}, .25)
-             .from('.teaser', 1.95, {scale: 2.5, autoRound: false, ease: 'power4.out'}, .05)
-             .from('.teaser-text h1', .7, {y: 20, opacity: 0, ease: 'power4.out'}, 1.2)
-             .from('.teaser-text p', .7, {y: 20, opacity: 0, ease: 'power4.out'}, 1.45)
-             .from('.screen', .65, {y: 200, opacity: 0, ease: 'power4.out'}, 1.7)
-             .from('.train', .65, {y: 200, opacity: 0, ease: 'power4.out'}, 1.95);
+        gsap.to('.page', 0, { visibility: 'visible' });
+        tl.from('.teaser-wrapper', 1.5, { y: 500, opacity: 0, ease: 'power4.out' }, .25)
+            .from('.teaser', 1.95, { scale: 2.5, autoRound: false, ease: 'power4.out' }, .05)
+            .from('.teaser-text h1', .7, { y: 20, opacity: 0, ease: 'power4.out' }, 1.2)
+            .from('.teaser-text p', .7, { y: 20, opacity: 0, ease: 'power4.out' }, 1.45)
+            .from('.screen', .65, { y: 200, opacity: 0, ease: 'power4.out' }, 1.7)
+            .from('.train', .65, { y: 200, opacity: 0, ease: 'power4.out' }, 1.95);
     }, []);
 
     return (
@@ -64,53 +64,53 @@ const Home = () => {
             <Helmet>
                 <title>Home - {configuration.appTitle}</title>
             </Helmet>
-            <Teaser teaser="An AI tool for early screening of COVID-19 & pneumonia from ultrasound recordings (POCUS)"/>
+            <Teaser teaser="An AI tool for early screening of COVID-19 & pneumonia from ultrasound recordings (POCUS)" />
             <section className="start container">
                 <div className="row">
                     <div
                         className="screen col-10 offset-1 col-sm-12 mb-5 mb-md-0 offset-sm-0 col-md-6 col-lg-5 offset-lg-1">
                         <CallToAction onClick={showDisclaimerModal} title="Screen"
-                                      text="Use our AI to detect COVID-19, pneumonia or healthy patient from POCUS data."
-                                      linkTitle="Start screening" className="primary"/>
+                            text="Use our AI to detect COVID-19, pneumonia or healthy patient from POCUS data."
+                            linkTitle="Start screening" className="primary" />
                     </div>
                     <div className="train col-10 offset-1 col-sm-12 offset-sm-0 col-md-6 col-lg-5">
                         <CallToAction action="/train" title="Donate your data"
-                                      text="Send us your POCUS data & recordings to train our AI about COVID-19, pneumonia or healthy patient."
-                                      linkTitle="Start training" className="secondary"/>
+                            text="Send us your POCUS data & recordings to train our AI about COVID-19, pneumonia or healthy patient."
+                            linkTitle="Start training" className="secondary" />
                     </div>
                 </div>
                 <div className="row">
                     <div
                         className="screen col-10 offset-1 col-sm-12 mb-5 mb-md-0 offset-sm-0 col-md-6 col-lg-5 offset-lg-1">
                         <CallToAction onClick={showDisclaimerModalVideo} title="Screen Video"
-                                      text="Use our AI to detect COVID-19, pneumonia or healthy patient from POCUS videos and view a heatmap of what the AI sees."
-                                      linkTitle="Start video screening" className="primary"/>
+                            text="Use our AI to detect COVID-19, pneumonia or healthy patient from POCUS videos and view a heatmap of what the AI sees."
+                            linkTitle="Start video screening" className="primary" />
                     </div>
                 </div>
             </section>
             <section className="container spacer">
                 <Text title="Our sponsors"
-                      text="We would like to address a special thank you to all of our sponsors. They are helping us with hosting, data collection, promotion and so on. Without them we would never have achieved what we did."/>
+                    text="We would like to address a special thank you to all of our sponsors. They are helping us with hosting, data collection, promotion and so on. Without them we would never have achieved what we did." />
                 <div className="row">
                     <div className="col-lg-10 offset-lg-1">
-                        <PartnerList/>
+                        <PartnerList />
                     </div>
                 </div>
             </section>
             <section className="container spacer">
                 <Text title="Statistics"
-                      text="Here are some statistics about our infrastructure. You can find the number of COVID-19 cases detected by the AI, the size of our dataset used to donate data and the number of organizations that are working with us."/>
-                <StatisticList/>
+                    text="Here are some statistics about our infrastructure. You can find the performance of our model in detecting COVID-19 pneumonia and the size of our dataset." />
+                <StatisticList />
             </section>
             <section className="container spacer pb-3">
-                <VideoPresentation title="Presentation video" videoId="1hJaVLGvzng"/>
+                <VideoPresentation title="Presentation video" videoId="1hJaVLGvzng" />
             </section>
             <section className="container spacer pt-4">
                 <Text title="The team"
-                      text="We are all very proud of our collaboration, mixing a pediatrician, a medical geneticist, a deep learning and drug discovery specialist, a medical biophysics and data specialist, an economist data scientist and an actuary data scientist. Everyone brought major contribution to this project and we believe we found something that could actually help COVID-19 detection and therefore improve treatments when given at early stage."/>
+                    text="We are all very proud of our collaboration, mixing a pediatrician, a medical geneticist, a deep learning and drug discovery specialist, a medical biophysics and data specialist, an economist data scientist and an actuary data scientist. Everyone brought major contribution to this project and we believe we found something that could actually help COVID-19 detection and therefore improve treatments when given at early stage." />
                 <div className="row">
                     <div className="col-lg-10 offset-lg-1">
-                        <Slider/>
+                        <Slider />
                     </div>
                 </div>
             </section>
@@ -118,7 +118,7 @@ const Home = () => {
                 <div className="row">
                     <div className="col-lg-10 offset-lg-1">
                         <TextImage orientation="textLeft" title="Early screening" subtitle="The impact" image={covid}
-                                   text="Early screening enables to save life by starting the therapy earlier and avoiding people to continue to contaminate others."/>
+                            text="Early screening enables to save life by starting the therapy earlier and avoiding people to continue to contaminate others." />
                     </div>
                 </div>
             </section>
@@ -126,7 +126,7 @@ const Home = () => {
                 <div className="row">
                     <div className="col-lg-10 offset-lg-1">
                         <TextImage title="Why POCUS?" subtitle="Focusing" image={pocus}
-                                   text="We focused on POCUS data, because ultrasound imaging is an easy, cost-effective and non-invasive method. It is available in almost any medical facility. Our model can identify with a 96% recall and 88% precision COVID-19 cases."/>
+                            text="We focused on POCUS data, because ultrasound imaging is an easy, cost-effective and non-invasive method. It is available in almost any medical facility. Our model can identify with a 96% recall and 88% precision COVID-19 cases." />
                     </div>
                 </div>
             </section>
