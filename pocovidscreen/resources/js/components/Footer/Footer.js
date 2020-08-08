f3b03a0cfaf016a6ebef3430a4c5cf9748a3aaa4
Jannis Born <jab@zurich.ibm.com>
2020-08-08 14:55:23 +0200

    wip: Add blogpost

--- pocovidscreen/resources/js/components/Footer/Footer.js
+++ pocovidscreen/resources/js/components/Footer/Footer.js
@@ -24,8 +24,8 @@ const Footer = () => {
                                 <h3 className="mb-4">Handy links</h3>
                                 <ul className="app-footer-links">
                                     <li><a href="https://arxiv.org/abs/2004.12084" title="Read our scientific article" target="_blank">Scientific article</a></li>
-                                    <li><a href="https://github.com/jannisborn/covid19_pocus_ultrasound" target="_blank" title="Find out how we achieved this">GitHub repository</a></li>
-                                    <li><a href="https://devpost.com/software/automatic-detection-of-covid-19-from-pocus-ultrasound-data" target="_blank" title="Read our DevPost">DevPost</a></li>
+                                    <li><a href="https://github.com/jannisborn/covid19_pocus_ultrasound" target="_blank" title="See code and data">GitHub repository</a></li>
+                                    <li><a href="https://towardsdatascience.com/ultrasound-for-covid-19-a-deep-learning-approach-f7906002892a" target="_blank" title="Read our blog post">Blog post</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -37,8 +37,8 @@ const Footer = () => {
                     <div className="row justify-content-between">
                         <div className="col">© Copyright {(new Date().getFullYear())}</div>
                         <div className="col text-right">A <a href="https://www.codevscovid19.org/" target="_blank"
-                                                             title="Visit the official event page">#CodeVsCovid19</a>, <a href="https://euvsvirus.org/" target="_blank"
-                                                                                                                          title="Visit the official event page">#EUvsVirus</a>  project
+                            title="Visit the official event page">#CodeVsCovid19</a>, <a href="https://euvsvirus.org/" target="_blank"
+                                title="Visit the official event page">#EUvsVirus</a>  project
                         </div>
                     </div>
                 </div>
